e89ce6d3f614a99e79bb6b509ae9ef5e366340cb
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:54:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Peruslaskutoimitukset(int a, int b)
+    {
+        int summa = a + b;
+        int erotus = a - b;
+        int tulo = a * b;
+        double jako = (double)a / b;
+
+        Console.WriteLine($"Summa: {summa}");
+        Console.WriteLine($"Erotus: {erotus}");
+        Console.WriteLine($"Tulo: {tulo}");
+        Console.WriteLine($"Jakolasku: {jako}");
+    }
+
+    static void Main()
+    {
+        Console.Write("Syötä ensimmäinen numero: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Syötä toinen numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Peruslaskutoimitukset(numero1, numero2);
+    }
+}
